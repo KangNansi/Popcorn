b795fea822d8d29f2f4643f81d160e86e3d0b8dc
PC-MARC\Marc <m21.cerutti@gmail.com>
2017-01-21 04:07:19 +0100

    no message

--- Assets/Script/RotationPopocorn.cs
+++ Assets/Script/RotationPopocorn.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class RotationPopocorn : MonoBehaviour {
 
+	public float coefV;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		this.transform.Rotate ( Vector3.up * coefV * Time.deltaTime);
 	}
 }
---
+++ Assets/Script/SceneManager/LvelsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvelsManager : MonoBehaviour {
+
+	public AudioSource backMusic;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	//Menu Acceuil
+
+	public void playLevel () {
+		
+		SceneManager.LoadScene ("Scene1", LoadSceneMode.Single);
+	}
+	public void playCredits() {
+
+		SceneManager.LoadScene ("SceneCredits", LoadSceneMode.Single);
+	}
+	public void quit() {
+
+		Application.Quit ();
+	}
+
+
+}
